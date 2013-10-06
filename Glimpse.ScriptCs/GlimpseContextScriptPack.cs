b308829dc3dd8a5095292d054882b0ef5376c041
Filip W <wojcieszyn@gmail.com>
2013-10-06 10:08:50 +0200

    Separated GlimpsePack and GlimpseContextScriptPack into dedicated files

---
+++ Glimpse.ScriptCs/GlimpseContextScriptPack.cs
@@ -0,0 +1,32 @@
+﻿using Glimpse.Core.Extensibility;
+using ScriptCs.Contracts;
+
+namespace Glimpse.ScriptCs
+{
+    public class GlimpseContextScriptPack : IScriptPack
+    {
+        private readonly GlimpsePack _ctx;
+
+        public GlimpseContextScriptPack(ITabContext ctx)
+        {
+            _ctx = new GlimpsePack(ctx);
+        }
+
+        public void Initialize(IScriptPackSession session)
+        {
+            session.ImportNamespace("Glimpse.ScriptCs");
+            session.ImportNamespace("System.Web");
+            session.ImportNamespace("Glimpse.AspNet.Extensions");
+        }
+
+        public IScriptPackContext GetContext()
+        {
+            return _ctx;
+        }
+
+        public void Terminate()
+        {
+            
+        }
+    }
+}
---
+++ Glimpse.ScriptCs/GlimpsePack.cs
@@ -0,0 +1,15 @@
+﻿using Glimpse.Core.Extensibility;
+using ScriptCs.Contracts;
+
+namespace Glimpse.ScriptCs
+{
+    public class GlimpsePack : IScriptPackContext
+    {
+        public ITabContext Context { get; private set; }
+
+        public GlimpsePack(ITabContext ctx)
+        {
+            Context = ctx;
+        }
+    }
+}
